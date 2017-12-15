21dce7cd06dc6114eb116df31987d5a9a2a0ac17
fernandoMontes <fernando@element.in>
2017-12-15 10:39:38 +0100

    removed bower dependency

---
+++ assets.js
@@ -0,0 +1,15 @@
+
+const CSS = [
+    'bootstrap/dist/css/bootstrap.css'
+];
+const JS = [
+	'masonry-layout/dist/masonry.pkgd.js',
+    'jquery/dist/jquery.js',
+    'angular/angular.js',
+    'angular-resource/angular-resource.js',
+    'angular-route/angular-route.js',
+    'bootstrap/dist/js/bootstrap.js'
+
+];
+
+module.exports = [...JS, ...CSS];
